56ed38ae6f79de06784a3f9f0924112becadae7f
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-11-16 12:02:43 +0500

    Add All Files

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const app = initializeApp({
+  apiKey: "AIzaSyAz2t-SrFc9RHG8YptffvS28VFUTqOqEiY",
+  authDomain: "twitter-clone-f37d2.firebaseapp.com",
+  projectId: "twitter-clone-f37d2",
+  storageBucket: "twitter-clone-f37d2.firebasestorage.app",
+  messagingSenderId: "1046531084067",
+  appId: "1:1046531084067:web:fac429e590042a6127c696"
+});
+
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export { auth, firestore };
